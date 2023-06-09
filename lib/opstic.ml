f8d6507f5d77caa5428f0540bfdd72c1fd803233
Keigo Imai <keigo.imai@gmail.com>
2023-06-10 06:48:56 +0900

    callback style

--- lib/opstic.ml
+++ lib/opstic.ml
@@ -1,9 +1,6 @@
-type ('m, 'x) variant = { make_var : 'x -> 'm; call_var : 'm -> 'x option }
 type connection = Connected | Join | JoinCorrelation
 
-let () =
-  let spin = ref (assert false) in
-  !spin (`A (100, fun ep -> ()))
+module Id = Id
 
 module type Monadic = sig
   type _ t
@@ -26,7 +23,10 @@ module type Endpoint = sig
     unit io
 
   val receive :
-    t -> connection:connection -> role:string -> (string * payload) io
+    t ->
+    connection:connection ->
+    roles:string list ->
+    (string * string * payload) io
 
   val close : t -> unit
 end
@@ -60,6 +60,8 @@ struct
   type endpoint = Endpoint.t
   type payload = Endpoint.payload
 
+  let return = Io.return
+  let ( let* ) = Io.bind
   let create raw wit = { ep_raw = raw; ep_witness = Lin.create wit }
 
   module Lin = Lin
@@ -67,39 +69,41 @@ struct
   module Witness = struct
     type 'm inp_choice =
       | InpChoice : {
-          inp_choice_role : string;
-          inp_choice_label : string;
+          inp_choice_role : ('m, 'l) Rows.constr;
+          inp_choice_label : ('l, 'v * 'b t) Rows.constr;
           inp_choice_marshal : payload -> 'v;
-          inp_choice_variant : ('m, 'v * 'b t) variant;
           inp_choice_next_wit : 'b;
         }
           -> 'm inp_choice
 
     type 'm inp = {
-      inp_role : string;
-      inp_choices : (string, 'm inp_choice) Hashtbl.t;
+      inp_roles : string list;
+      inp_choices : (string * string, 'm inp_choice) Hashtbl.t;
       inp_connection : connection;
     }
       constraint 'm = [> ]
 
-    let make_inp ~role ?(conn = Connected) (xs : (string * 'm inp_choice) list)
-        : 'm inp =
+    let make_inp ?(conn = Connected) (xs : 'm inp_choice list) : 'm inp =
       let tbl = Hashtbl.create (List.length xs) in
       let rec put_all = function
-        | (k, v) :: xs ->
-            Hashtbl.add tbl k v;
+        | (InpChoice c0 as c) :: xs ->
+            Hashtbl.add tbl
+              (c0.inp_choice_role.constr_name, c0.inp_choice_label.constr_name)
+              c;
             put_all xs
         | [] -> ()
       in
       put_all xs;
-      { inp_role = role; inp_connection = conn; inp_choices = tbl }
+      let roles =
+        xs |> List.map (fun (InpChoice c0) -> c0.inp_choice_role.constr_name)
+      in
+      { inp_roles = roles; inp_connection = conn; inp_choices = tbl }
 
     type 'm out_choice =
       | OutChoice : {
-          out_choice_role : string;
-          out_choice_label : string;
+          out_choice_role : ('m, 'l) Rows.constr;
+          out_choice_label : ('l, 'v * ('b t -> unit io)) Rows.constr;
           out_choice_marshal : 'v -> payload;
-          out_choice_variant : ('m, 'v * ('b t -> unit)) variant;
           out_choice_next_wit : 'b;
         }
           -> 'm out_choice
@@ -108,13 +112,10 @@ struct
       out_choices : 'm out_choice list;
       out_connection : connection;
     }
+      constraint 'm = [> ]
 
-    (* let make_out ?(con = Connected)
-        (xs : 'm choice list) : 'm out =
-        {
-          out_choices = List.map (fun (role,label,choice))
-          out_connection=Connected
-        } *)
+    let make_out ?(conn = Connected) (xs : 'm out_choice list) : 'm out =
+      { out_connection = conn; out_choices = xs }
   end
 
   module Comm = struct
@@ -122,41 +123,43 @@ struct
     type 'm out = 'm Witness.out
     type 'a ep = 'a t
 
-    let send : 'm out ep -> 'm -> unit io =
+    let send : 'm. 'm out ep -> 'm -> unit io =
      fun ep msg ->
-      let out : 'm out = Lin.get ep.ep_witness in
+      let out = Lin.get ep.ep_witness in
       let rec loop = function
         | Witness.OutChoice c :: cs -> (
-            match c.out_choice_variant.call_var msg with
-            | Some (v, f) ->
-                Io.bind
-                  (Endpoint.send ep.ep_raw ~connection:out.out_connection
-                     ~role:c.out_choice_role ~label:c.out_choice_label
-                     ~payload:(c.out_choice_marshal v))
-                  (fun () ->
-                    Io.return
-                    @@ f
-                         {
-                           ep with
-                           ep_witness = Lin.create c.out_choice_next_wit;
-                         })
-            | None -> loop cs)
+            match
+              Option.bind
+                (c.out_choice_role.match_var msg)
+                c.out_choice_label.match_var
+            with
+            | None -> loop cs
+            | Some (v, callback) ->
+                let* () =
+                  Endpoint.send ep.ep_raw ~connection:out.out_connection
+                    ~role:c.out_choice_role.constr_name
+                    ~label:c.out_choice_label.constr_name
+                    ~payload:(c.out_choice_marshal v)
+                in
+                callback
+                  { ep with ep_witness = Lin.create c.out_choice_next_wit })
         | [] -> failwith ""
       in
       loop out.out_choices
 
-    let receive : ([> ] as 'a) inp ep -> ('a -> unit) -> unit io =
-     fun ep f (*fun x -> x#a*) ->
+    let receive : ([> ] as 'a) inp ep -> ('a -> unit io) -> unit io =
+     fun ep callback ->
       let inp = Lin.get ep.ep_witness in
-      Io.bind
-        (Endpoint.receive ep.ep_raw ~role:inp.inp_role
-           ~connection:inp.inp_connection) (fun (label, v) ->
-          let (InpChoice c) = Hashtbl.find inp.inp_choices label in
-          let v = c.inp_choice_marshal v in
-          Io.return
-          @@ f
-               (c.inp_choice_variant.make_var (*fun x -> `lab x*)
-                  (v, { ep with ep_witness = Lin.create c.inp_choice_next_wit })))
+      let* role, label, v =
+        Endpoint.receive ep.ep_raw ~roles:inp.inp_roles
+          ~connection:inp.inp_connection
+      in
+      let (InpChoice c) = Hashtbl.find inp.inp_choices (role, label) in
+      let v = c.inp_choice_marshal v in
+      callback
+        (c.inp_choice_role.make_var
+           (c.inp_choice_label.make_var
+              (v, { ep with ep_witness = Lin.create c.inp_choice_next_wit })))
 
     let close (ep : unit ep) =
       ignore @@ Lin.get ep.ep_witness;
--- lib_js/opstic_js.ml
+++ lib_js/opstic_js.ml
@@ -2,44 +2,49 @@ open! Kxclib
 include Types
 module ServerIo = ServerIo
 module ConcurrentQueue = ConcurrentQueue
-module Server = Server
 
-let handle_request (server : Server.t) (request : Json.jv) : Json.jv ServerIo.t
-    =
-  let open ServerIo in
-  let ( let* ) = ServerIo.bind in
-  let ( let+ ) m f = ServerIo.map f m in
-  let access fld msg =
-    match Jv.access [ `f fld ] request with
-    | Some (`str x) -> return x
-    | _ -> error_with msg
-  in
-  let* entrypoint_id =
-    access "entrypoint" "entrypoint not given" |> map EntrypointId.create
-  in
-  let* role = access "role" "role not given" |> map Role.create in
-  let conversation_id () =
-    access "conversation_id" "conversation_id not given"
-    |> map ConversationId.create
-  in
-  let http_session_id () =
-    access "session_id" "session_id not given" |> map SessionId.create
-  in
-  let* mode_str = access "mode" "mode not given" in
-  let* entrypoint_kind =
-    let open Server.Handler in
-    match mode_str with
-    | "start" -> return (StartLeader role)
-    | "start_follower" ->
-        let+ conversation_id = conversation_id () in
-        StartFollower (role, conversation_id)
-    | "join" -> return (Join role)
-    | "join_correlation" ->
-        let+ conversation_id = conversation_id () in
-        JoinCorrelation (role, conversation_id)
-    | "session" ->
-        let+ http_session_id = http_session_id () in
-        InSession http_session_id
-    | _ -> error_with (Format.asprintf "wrong mode: %s" mode_str)
-  in
-  Server.Handler.handle_entry server ~entrypoint_id ~entrypoint_kind ~request
+module Server = struct
+  include Server
+
+  let handle_request (server : Server.t) (request : Json.jv) :
+      Json.jv ServerIo.t =
+    let open ServerIo in
+    let ( let* ) = ServerIo.bind in
+    let ( let+ ) m f = ServerIo.map f m in
+    let access fld msg =
+      match Jv.access [ `f fld ] request with
+      | Some (`str x) -> return x
+      | _ -> error_with msg
+    in
+    let* entrypoint_id =
+      access "entrypoint" "entrypoint not given" |> map EntrypointId.create
+    in
+    let* role = access "role" "role not given" |> map Role.create in
+    let conversation_id () =
+      access "conversation_id" "conversation_id not given"
+      |> map ConversationId.create
+    in
+    let http_session_id () =
+      access "session_id" "session_id not given" |> map SessionId.create
+    in
+    let* mode_str = access "mode" "mode not given" in
+    let* entrypoint_kind =
+      let open Server.Handler in
+      match mode_str with
+      | "start" -> return (StartLeader role)
+      | "start_follower" ->
+          let+ conversation_id = conversation_id () in
+          StartFollower (role, conversation_id)
+      | "join" -> return (Join role)
+      | "join_correlation" ->
+          let+ conversation_id = conversation_id () in
+          JoinCorrelation (role, conversation_id)
+      | "session" ->
+          let+ http_session_id = http_session_id () in
+          InSession http_session_id
+      | _ -> error_with (Format.asprintf "wrong mode: %s" mode_str)
+    in
+    Server.Handler.handle_entry server ~entrypoint_id ~entrypoint_kind ~request
+end
+
+module Mpst_js = struct end
