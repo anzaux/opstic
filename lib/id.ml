1cf2db3a28a21e3eb5643929aa5bed1dcced00fb
Keigo Imai <keigo.imai@gmail.com>
2023-06-09 20:57:44 +0900

    factor out JS server

--- lib/id.ml
+++ lib/id.ml
@@ -1,9 +1,11 @@
-module Make () : sig
+module type S = sig
   type t [@@deriving show]
 
   val to_string : t -> string
   val create : string -> t
-end = struct
+end
+
+module Make () : S = struct
   type t = string [@@deriving show]
 
   let to_string x = x
